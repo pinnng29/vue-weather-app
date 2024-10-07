a191518284e035878e17c5ee1f9f2375250bce25
pinnng29 <apingspeedholic@gmail.com>
2024-10-07 09:06:36 +0700

    reusable modal

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -11,16 +11,57 @@
       </RouterLink>
       <div class="flex gap-3 flex-1 justify-end">
         <i
+          @click="toggleModal"
           class="fa-solid fa-circle-info text-2xl hover:text-weather-secondary duration-150 hover:cursor-pointer"
         />
         <i
           class="fa-solid fa-plus text-2xl hover:text-weather-secondary duration-150 hover:cursor-pointer"
         />
       </div>
+
+      <BaseModal
+        :modalActive="modalActive"
+        @close-modal="toggleModal"
+      >
+        <div class="text-black">
+          <h1 class="text-2xl mb-1">About:</h1>
+          <p class="mb-2">
+            The Local Weather allow you to track the current and future weather
+            of cities of your choosing.
+          </p>
+          <h2 class="text-2xl">How it work:</h2>
+          <ol class="list-decimal list-inside mb-4">
+            <li>
+              search for your city by entering the name into the search bar.
+            </li>
+            <li>
+              Select a city within the results this will take you to the current
+              weather for your selection.
+            </li>
+            <li>
+              Track the city by clicking on the "+" icon in the top right. This
+              will save the city to view at a later time on the home page.
+            </li>
+          </ol>
+          <h2 class="text-2xl">Removing the city</h2>
+          <p>
+            If you no longer wish to track a city, simply select the city within
+            the home page. At the bottom of the page, there will be am option to
+            delete the city.
+          </p>
+        </div>
+      </BaseModal>
     </nav>
   </header>
 </template>
 
 <script setup>
 import { RouterLink } from "vue-router";
+import BaseModal from "./BaseModal.vue";
+import { ref } from "vue";
+
+const modalActive = ref(null);
+const toggleModal = () => {
+  modalActive.value = !modalActive.value;
+};
 </script>
